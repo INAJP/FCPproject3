6220c23bf2731bddf601260fb61f20ce64755009
INAJP <alialsorezabody@gmail.com>
2025-01-26 16:41:42 +0330

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct students {
+    string name;
+    int userid;
+    string major;
+};
+
+struct Course {
+    string coursename;
+    float grade;
+    int credits;
+};
